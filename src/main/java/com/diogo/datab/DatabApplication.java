e59b8ca0f647f41f69ed4803be62678d6b882f4e
DiogoCaverna <caverna666@outlook.com>
2024-04-30 09:33:12 -0300

    mvn clean package working

---
+++ src/main/java/com/diogo/datab/DatabApplication.java
@@ -0,0 +1,13 @@
+package com.diogo.datab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabApplication.class, args);
+	}
+
+}
